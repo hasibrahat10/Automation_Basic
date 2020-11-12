fd26231e61e31ae4b5fe901944795f7f881c3087
hasibrahat10 <hasibrahat10@gmail.com>
2020-11-12 12:44:39 +0600

    Environment setup

---
+++ src/main/java/stepDefinitions/ThisHeartsteps.java
@@ -0,0 +1,37 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.Assert;
+import org.testng.Assert;
+
+public class ThisHeartsteps {
+    WebDriver driver;
+
+    @Given("I launch {int} chrome browser")
+    public void i_launch_chrome_browser(int chrome) {
+        System.out.println(chrome);
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver(); // launch browser
+    }
+
+    @When("I open thisheart homepage")
+    public void i_open_thisheart_homepage() {
+        driver.get("https://thisheart.co/");
+    }
+
+    @Then("I verify the logo present on the page")
+    public void i_verify_the_logo_present_on_the_page() {
+       boolean status = driver.findElement(By.xpath("//*[@id=\"header\"]/div/nav/a/img")).isDisplayed();
+        Assert.assertEquals(true, status);
+    }
+
+    @Then("close browser")
+    public void close_browser() {
+        driver.quit();
+    }
+}
